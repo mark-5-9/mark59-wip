d5b78643d4f71c96b0159ae2f91cbfa219c21866
mark-5-9 <52810316+mark-5-9@users.noreply.github.com>
2021-05-03 14:35:23 +1000

    3.2 candidate

--- bin/UtilityCreateMark59Zip.sh
+++ bin/UtilityCreateMark59Zip.sh
@@ -20,7 +20,7 @@ rm -rf ${DEST_DIR}
 mkdir -p ${DEST_DIR}
 cd ~/gitrepo/mark59-wip/
 
-rsync -av -m --exclude '.*' --exclude '*/src' --exclude '*/webapp' --exclude '*/test' --exclude '*/WEB-INF' --exclude '*/classes' --exclude '*/test-classes' --exclude 'TESTDATA' --exclude '*/maven*' --exclude '*/m2e-wtp*' --exclude '*/surefire*' --exclude '*.log' --exclude '*.png' --exclude '*.gif' --exclude '*.original' --exclude '*.java' --exclude 'UtilityCreateMark59Zip.*' --exclude 'pom.xml' --exclude 'mark59-server-metrics-web.jar' --exclude 'metrics.jar' --exclude 'dataHunterPerformanceTestSamples' --exclude 'mark59-selenium-sample-dsl' --exclude 'mark59-core' --exclude 'mark59-selenium-implementation' "${SOURCE_DIR}" "${DEST_DIR}"
+rsync -av -m --exclude '.*' --exclude '*/src' --exclude '*/webapp' --exclude '*/test' --exclude '*/WEB-INF' --exclude '*/classes' --exclude '*/test-classes' --exclude 'TESTDATA' --exclude '*/maven*' --exclude 'archive-tmp' --exclude '*/m2e-wtp*' --exclude '*/surefire*' --exclude '*.log' --exclude '*.png' --exclude '*.gif' --exclude '*.original' --exclude '*.java' --exclude 'UtilityCreateMark59Zip.*' --exclude 'pom.xml' --exclude 'mark59-server-metrics-web.jar' --exclude 'metrics.jar' --exclude 'dataHunterPerformanceTestSamples' --exclude 'mark59-selenium-sample-dsl' --exclude 'mark59-core' --exclude 'mark59-selenium-implementation' "${SOURCE_DIR}" "${DEST_DIR}"
 
 
 # In the command below, for a _PRE_RELEASE version: do NOT include the directives "--exclude 'mark59-core' --exclude 'mark59-selenium-implementation' " 
@@ -28,6 +28,6 @@ rsync -av -m --exclude '.*' --exclude '*/src' --exclude '*/webapp' --exclude '*/
 # When building the full _RELEASE zip file, add the directives in. That is, the end of the command should look like :
 #      ... --exclude 'mark59-core' --exclude 'mark59-selenium-implementation' "${SOURCE_DIR}" "${DEST_DIR}"  
 
-rsync -a -m --exclude '.*' --exclude '*/classes' --exclude '*/test-classes' --exclude '*/maven*' --exclude '*/m2e-wtp*' --exclude '*/surefire*' --exclude '*.log' --exclude '*.original' --exclude '/pom.xml' --exclude 'bin' --exclude 'databaseScripts' --exclude 'dataHunter' --exclude 'mark59-server-metrics' --exclude 'mark59-server-metrics-web' --exclude 'metrics' --exclude 'metricsRuncheck' --exclude 'resultFilesConverter' --exclude 'mark59-core' --exclude 'mark59-selenium-implementation' "${SOURCE_DIR}" "${DEST_DIR}"
+rsync -a -m --exclude '.*' --exclude '*/classes' --exclude '*/test-classes' --exclude '*/maven*' --exclude 'archive-tmp' --exclude '*/m2e-wtp*' --exclude '*/surefire*' --exclude '*.log' --exclude '*.original' --exclude '/pom.xml' --exclude 'bin' --exclude 'databaseScripts' --exclude 'dataHunter' --exclude 'mark59-server-metrics' --exclude 'mark59-server-metrics-web' --exclude 'metrics' --exclude 'metricsRuncheck' --exclude 'resultFilesConverter' --exclude 'mark59-core' --exclude 'mark59-selenium-implementation' "${SOURCE_DIR}" "${DEST_DIR}"
 
 $SHELL

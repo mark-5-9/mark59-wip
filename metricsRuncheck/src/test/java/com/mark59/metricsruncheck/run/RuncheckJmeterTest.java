1baca0b78726787631cbeca5ad5bbfdee5b9e90a
s62991 <philip.webb@iag.com.au>
2021-06-11 16:50:07 +1000

    junit cleanup

--- metricsRuncheck/src/test/java/com/mark59/metricsruncheck/run/RuncheckJmeterTest.java
+++ metricsRuncheck/src/test/java/com/mark59/metricsruncheck/run/RuncheckJmeterTest.java
@@ -2,39 +2,17 @@ package com.mark59.metricsruncheck.run;
 
 import java.util.List;
 
-import javax.sql.DataSource;
-
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import com.mark59.core.utils.Mark59Constants;
-import com.mark59.metrics.data.application.dao.ApplicationDAO;
-import com.mark59.metrics.data.application.dao.ApplicationDAOjdbcTemplateImpl;
 import com.mark59.metrics.data.beans.Transaction;
-import com.mark59.metrics.data.eventMapping.dao.EventMappingDAO;
-import com.mark59.metrics.data.eventMapping.dao.EventMappingDAOjdbcTemplateImpl;
-import com.mark59.metrics.data.graphMapping.dao.GraphMappingDAO;
-import com.mark59.metrics.data.graphMapping.dao.GraphMappingDAOjdbcTemplateImpl;
-import com.mark59.metrics.data.metricSla.dao.MetricSlaDAO;
-import com.mark59.metrics.data.metricSla.dao.MetricSlaDAOjdbcImpl;
-import com.mark59.metrics.data.run.dao.RunDAO;
-import com.mark59.metrics.data.run.dao.RunDAOjdbcTemplateImpl;
-import com.mark59.metrics.data.sla.dao.SlaDAO;
-import com.mark59.metrics.data.sla.dao.SlaDAOjdbcImpl;
-import com.mark59.metrics.data.testTransactions.dao.TestTransactionsDAO;
-import com.mark59.metrics.data.testTransactions.dao.TestTransactionsDAOjdbcTemplateImpl;
-import com.mark59.metrics.data.transaction.dao.TransactionDAO;
-import com.mark59.metrics.data.transaction.dao.TransactionDAOjdbcTemplateImpl;
 import com.mark59.metrics.metricSla.MetricSlaResult;
 import com.mark59.metrics.sla.SlaTransactionResult;
 import com.mark59.metricsruncheck.Runcheck;
@@ -42,65 +20,13 @@ import com.mark59.metricsruncheck.Runcheck;
 import junit.framework.TestCase;
 
 
-
 public class RuncheckJmeterTest extends TestCase {
 
-	@Autowired
-	DataSource dataSource;
-    @Autowired
-    String currentDatabaseProfile;  
-	@Autowired
-	MetricSlaDAO metricSlaDAO;
-	@Autowired
-	TransactionDAO transactionDAO;
-	@Autowired
-	SlaDAO slaDAO;
-	@Autowired
-	RunDAO runDAO;
-	@Autowired
-	TestTransactionsDAO testTransactionsDAO;
-	@Autowired
-	EventMappingDAO eventMappingDAO;
-	@Autowired
-	ApplicationContext context;
-	
-	@SuppressWarnings("rawtypes")
-	@Bean
-	public DataSource dataSource() {
-		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-		dataSourceBuilder.driverClassName("org.h2.Driver");
-		dataSourceBuilder.url("jdbc:h2:mem:metricsmem;MODE=MySQL");
-		dataSourceBuilder.username("SA");
-		dataSourceBuilder.password("");
-		return dataSourceBuilder.build();
-	};
-		
-    @Value("h2mem")
-    private String springProfilesActive;	
-	@Bean
-    public String currentDatabaseProfile() {return "h2mem";   }   
-	@Bean
-	public ApplicationDAO applicationDAO() {return new ApplicationDAOjdbcTemplateImpl();}
-	@Bean
-	public RunDAO runDAO() {return new RunDAOjdbcTemplateImpl();}
-	@Bean
-	public TransactionDAO transactionDAO() {return new TransactionDAOjdbcTemplateImpl();}
-	@Bean
-	public SlaDAO slaDAO() {return new SlaDAOjdbcImpl();}
-	@Bean
-	public MetricSlaDAO metricSlaDAO() {return new MetricSlaDAOjdbcImpl();}
-	@Bean
-	public GraphMappingDAO graphMappingDAO() {	return new GraphMappingDAOjdbcTemplateImpl();}
-	@Bean
-	public EventMappingDAO eventMappingDAO() {	return new EventMappingDAOjdbcTemplateImpl();}
-	@Bean
-	public TestTransactionsDAO testTransactionsDAO() {return new TestTransactionsDAOjdbcTemplateImpl();	}
-
 	EmbeddedDatabase db; 
+	ApplicationContext context;
 
 	public void setUp() {
 		db = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).setName("metricsmem;MODE=MySQL;").addScript("copyofschema.sql").build();
-		// for multiple tests?:  DB_CLOSE_DELAY=-1;")
 	}
 	
 	@Test
